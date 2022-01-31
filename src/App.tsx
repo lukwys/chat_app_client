6186bcf559bff191ec7c4eaa0e4d5ee3eec75960
lwyso <lwyso@softserveinc.com>
2022-01-31 22:36:50 +0100

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MessageField } from "./components/MessageField";
+import { MessageInput } from "./components/MessageInput";
+import { Container } from "react-bootstrap";
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="app text-center">
+      <h1>Chat app</h1>
+      <MessageField />
+      <MessageInput />
+    </Container>
   );
 }
 
---
+++ src/components/MessageField.tsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export const MessageField = () => {
+  return (
+    <Container className="border border-light">
+      <p className="text-start">Example message</p>
+    </Container>
+  );
+};
